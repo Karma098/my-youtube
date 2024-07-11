200dc5d6b2d576cbc096e8a898a2385e4ed38023
karma098 <kumarhritihik804@gmail.com>
2024-07-11 18:35:53 +0530

    searchAPI, comment section

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,10 +1,51 @@
-import React from "react";
-import { MENU_ICON, USER_ICON, YOUTUBE_ICON } from "../utils/constants";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import {
+  MENU_ICON,
+  USER_ICON,
+  YOUTUBE_ICON,
+  YOUTUBE_SEARCH_API,
+} from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
+import { cacheResults } from "../utils/searchSlice";
 
 const Head = () => {
+  const [suggestions, setSuggestions] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  const searchCache = useSelector((store) => store.search);
   const dispatch = useDispatch();
+  // console.log(searchQuery);
+  useEffect(() => {
+    //API call
+    //make an api call after eveyr key press
+    //but if the difference between 2 API calls is <200ms
+    //decline the API call
+    const timer = setTimeout(() => {
+      if (searchCache[searchQuery]) {
+        setSuggestions(searchCache[searchQuery]);
+      } else {
+        getSearchSuggestion();
+      }
+    }, 200);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestion = async () => {
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    // console.log(json[1]);
+    setSuggestions(json[1]);
+    dispatch(
+      cacheResults({
+        [searchQuery]: json[1],
+      })
+    );
+  };
+
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
   };
@@ -20,13 +61,30 @@ const Head = () => {
         <img className="h-8 mx-2" alt="logo" src={YOUTUBE_ICON} />
       </div>
       <div className="col-span-10 px-10">
-        <input
-          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
-          type="text"
-        />
-        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
-          🔍
-        </button>
+        <div>
+          <input
+            className="px-5 w-1/2 border border-gray-400 p-2 rounded-l-full"
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
+          />
+          <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+            🔍
+          </button>
+        </div>
+        {showSuggestions && (
+          <div className="fixed bg-white py-2 px-5 w-[33rem] shadow-lg rounded-lg border border-gray-200">
+            <ul>
+              {suggestions.map((s) => (
+                <li key={s} className="py-2 shadow-sm hover:bg-gray-100">
+                  🔍{s}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
       <div className="col-span-1">
         <img className="h-8" alt="icon" src={USER_ICON} />
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,3 +10,6 @@ export const USER_ICON =
 const GOOGLE_API_KEY = "AIzaSyBnfHU4t0MIhcizIwLiMKpBH8gxAIqzMnw";
 
 export const YOUTUBE_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${GOOGLE_API_KEY}`;
+
+export const YOUTUBE_SEARCH_API =
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
